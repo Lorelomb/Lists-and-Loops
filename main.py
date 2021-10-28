f18757823a3fcc718932d34bf3c3947d5739ac18
Lorelomb <2020326@brunel.ac.uk>
2021-10-28 23:58:50 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# wordList = ['mix', 'xyz', 'apple', 'xanadu',"rovio"]
+# xwordList = []
+# noxwordList = []
+# newList = []
+#
+# for item in wordList:
+#     if "x" in item[0]:
+#         xwordList.append(item)
+#     else:
+#         noxwordList.append(item)
+#
+# xwordList.sort()
+# noxwordList.sort()
+#
+# print(xwordList)
+# print(noxwordList)
+# newList += xwordList + noxwordList
+# print(newList)
+
+
+wordList = ["xxx", "aaa", "r5t6yy", "g","wow"]
+
+len(wordList)
